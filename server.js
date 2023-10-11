77e144c9ac8c9ca79d57cd867a790ae3eceb2629
chx38895 <116450342+chx38895@users.noreply.github.com>
2023-10-11 03:54:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const pool = require("./controllers/dbContext")
 const app = express();
-const port = 8080; 
+const port = 3000;
 
 
 app.use(express.static(path.join(__dirname, "public")));
